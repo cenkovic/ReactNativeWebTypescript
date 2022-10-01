9c6f57f2d27d991da5ad20606cfc5ff55bf964fb
Nemanja Cenkovic <cenkovic.nemanja@gmail.com>
2022-10-01 11:29:42 +0200

    Renaming and CSS fix

--- ios/ReactNativeWebTypescriptTests/ReactNativeWebTypescriptTests.m
+++ ios/ReactNativeWebTypescriptTests/ReactNativeWebTypescriptTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GameNightAppTests : XCTestCase
+@interface ReactNativeWebTypescriptTests : XCTestCase
 
 @end
 
-@implementation GameNightAppTests
+@implementation ReactNativeWebTypescriptTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
